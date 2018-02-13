a93e2146045e8acaf379109e811259619e2642d2
ahmed2580 <three-star-computer@hotmail.com>
2018-02-13 10:58:18 +0100

    do it

---
+++ index.js
@@ -0,0 +1,37 @@
+// function viewImges(x){
+// 	var url	= "url(images/img0"+x+".png)";
+// 	var div = document.getElementById('img-id');
+// 	div.style = "border:2px solid #999";
+// 	div.innerHTML = "<img scr=\"iimages/img0"+x+".png\">";
+// }
+// function showImg(img){
+// 	var pic = document.getElementById("output");
+// 	pic.innerHTML = "<img src="images/" + img + ".png"/>";
+// }
+function changBg(x){
+	var url = "url(\"images/img0"+x+".png\")"
+	document.getElementById("screen").style.backgroundImage=url;
+}
+function btn(){
+document.getElementById("demo").innerHTML = (x <= 100);	
+}
+
+// var img0 = (\"images/img0"+x+".png
+// 	function right(n){
+// 	for (var i = 0 ; i<.length - 1; i >= 0; i--) {
+// 		0 ; i<[i]
+// 	}
+// 	n=0;
+// 	n+=5;
+// }
+// var button = document.getElementById("btn").addEventListener("mousemove",imgEvent);
+// function imgEvent(e){
+// 	var by
+// }
+// document.getElementById("demo").innerHTML = 8 + 6;
+// document.write(5+6);
+
+// var Things = [image1, image2];
+// for (var i = 0 i< Things.length; i >= 0; i--) {
+// 	Things[i]
+// }
